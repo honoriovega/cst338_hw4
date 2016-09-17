b0305492b58a6e93fac317f6d6eb5a191cee0272
sentinel <honoriovega@student.hartnell.edu>
2016-09-17 16:41:37 -0700

    add a new file

---
+++ Bank.java
@@ -0,0 +1,30 @@
+public class Bank 
+{
+	private String name;
+	private Account accounts[];
+	
+	// add more variables if necessary
+	
+	public Bank(String name)
+	{
+		this.name = name;
+	}
+	
+	boolean openAccount(String name, String addr, int ssn)
+	{
+	}
+	
+	boolean closeAccount(int accNum)
+	{
+	}
+	
+	boolean updateAddress(int accNum, String addr)
+	{
+	}
+	
+	void bankInfo()
+	{
+	}
+	
+	// if necessary add more methods
+}
